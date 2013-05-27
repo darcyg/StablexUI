08798800f31b8c21811c7a4a7257935c78489504
Darcy.G <darcy.gong@gmail.com>
2013-05-27 13:51:06 +0800

    add StablexUI haxelib.xml and haxelib.json

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
 StablexUI
 =========
 
-StablexUI Lib
+Macro driven ui engine for Haxe NME. It's designed to give developer as much freedom as possible in customizing UI.
+Supported targets: cpp (iOS, Android, Blackberry, webOS, Windows, Linux, MacOS), flash, html5, neko.
+
+Online docs for API and basic features: http://ui.stablex.ru/doc
+
+Demo with android-4.x-like theme (written completely in xml):
+* flash: http://ui.stablex.ru/demo/demo_flash/
+* html5: http://ui.stablex.ru/demo/demo_html5/
+
+Games prototypes:
+* Coder: all objects in this game are StablexUI widgets - play on [newgrounds](http://www.newgrounds.com/portal/view/616444); [sources](https://github.com/RealyUniqueName/LD26-Coder)
+* The Tale of Ladybird: all objects in this game are StablexUI widgets - http://stablex.ru/ladybird
+* Untwist: simple game with simple ui and screen transitions - http://ui.stablex.ru/demo/untwist/
+
+Another example:
+* flash: http://ui.stablex.ru/demo/handlers.swf
+* html5: http://ui.stablex.ru/demo/handlers/
+
+More simple demoes: http://ui.stablex.ru/demo
+
+
+Features:
+---------------
+* Easy integration with any project. Just addChild StablexUI to any Sprite or Stage.
+* Separate "view" from "controller". Create all UI via xml (but you still can create ui with plain haxe);
+* Compile time xml parsing. All xmls can be preprocessed at compile time, so your app is loading faster. Also everything is still strictly typed ;)
+* Run time xml parsing. You can update UI in already published apps without updating whole app.
+* Custom meta tags for xml to implement functionality you need, but wich is not implemented in StablexUI;
+* Writing handlers (plain haxe code) right in xml;
+* Really crossplatform! Works in cpp, flash, html5;
+* Easy Drag'n'Drop;
+* Relative positioning (you can define position  by any border of element - left, right, top, bottom in pixels or percentage of parent's size. Define width/height in percentage etc);
+* You can write haxe code right in attributes in xml to acces anything from anywhere! For example:
+```xml
+<Text text="'Hello world!'" x="nme.Lib.current.stage.stageWidth/2" />
+```
+* Easy skinning with 3-slice-scaling, 9-slice-scaling, tiling with texture, filling with color, gradient fill or your own custom skin processor;
+* Flexible layout system (including custom layouts);
+* Don't worry about forgotten eventListeners. Just use widget.free() method to let GC destroy the object;
+* Ability to create custom classes for skinning, transitions, tooltips, etc;
+* There is more! Some features are described in docs: http://ui.stablex.ru/doc;
+
+
+Implemented widgets:
+---------------
+* Text: normal and input;
+* Buttons: simple, toggle, multiple states;
+* Checkbox;
+* Radio;
+* Bitmaps;
+* VBox, HBox;
+* Scroll container;
+* Progress bar;
+* Tabs;
+* Tooltips;
+* Sliders;
+* Switch (on/off toggle);
+* Options box (picker list / drop-down)
+* View stacks (only one visible child at every moment);
+* Clock (display time, display constant time value, countdown);
+* More will be available soon ;)
+
+
+
+
+
---
+++ ru/stablex/ui/widgets/Image.hx
@@ -0,0 +1,254 @@
+package ru.stablex.ui.widgets;
+
+import nme.display.BitmapData;
+import mloader.Loader;
+import mloader.ImageLoader;
+import nme.events.Event;
+import ru.stablex.ui.events.WidgetEvent;
+import ru.stablex.ui.events.ImageWidgetEvent;
+import ru.stablex.ui.events.URLLoaderEvent;
+
+/**
+ * ...
+ * @author Darcy.G
+ */
+class Image extends Bmp
+{
+    
+    public var defaultSrc(get_defaultSrc, set_defaultSrc) : String;
+    private var _defaultSrc : String = null;
+    
+    private function get_defaultSrc() : String {
+        return this._defaultSrc;
+    }
+
+    private function set_defaultSrc(src : String) : String {
+        if (src != null && src != "") this._defaultSrc = src;
+        return this._defaultSrc;
+    }
+
+    public var errorSrc(get_errorSrc, set_errorSrc) : String;
+    private var _errorSrc : String = null;
+    
+    private function get_errorSrc() : String {
+        return this._errorSrc;
+    }
+
+    private function set_errorSrc(src : String) : String {
+        if (src != null && src != "") this._errorSrc = src;
+        return this._errorSrc;
+    }    
+    
+    private var _loader:ImageLoader = null;
+    private var _lastSrc : String = null;
+    private var _lastW : Float;
+    private var _lastH : Float;
+    
+    public var loading(get_loading, null) : Bool; 
+    private function get_loading() : Bool {
+        if (this._loader != null) {
+            return this._loader.loading;
+        }
+        return false;
+    }
+    
+    public var progress(get_progress, null) : Float;
+    private var _progress : Float = -1 ;
+    private function get_progress() : Float {
+        //if (this._loader != null) {
+            //this._progress = this._loader.progress;
+        //}
+        return this._progress;        
+    }
+    
+    public var loaded(get_loaded, null) : Bool;
+    private var _loaded : Bool = false;
+    private function get_loaded() : Bool {
+        return this._loaded;        
+    }
+    
+    private var _updateBitmapCache : Bool = false;
+    private var _islocalsrc : Bool = false;
+    
+    public var fixedSize : Bool = false;
+    
+    /**
+    * Setter src
+    *
+    */
+    override private function set_src(src:String) : String {
+        if( src != null ){
+            //this._bitmapData = null;
+            this._src = src;
+            if (src.indexOf("http://") == 0 || src.indexOf("https://") == 0) {
+                this._lastSrc = this._defaultSrc;
+                this._progress = 0;
+                this._loader = new ImageLoader(src);
+                this._loader.loaded.add(this.__onLoaded);
+                this._loader.load();
+                this._islocalsrc = false;
+            } else {
+                this._bitmapData = null;
+                this._lastSrc = src;
+                this._updateBitmapCache = true;
+                this._islocalsrc = true;
+                this.refresh();
+            }
+        }
+        return this._src ;//= src;
+    }//function set_src()
+    
+    /**
+    * Setter bitmapData
+    *
+    */
+    override private function set_bitmapData(bmp:BitmapData) : BitmapData {
+        if( bmp != null ){
+            this._lastSrc = null;
+        }
+        return this._bitmapData = bmp;
+    }//function set_bitmapData()    
+    
+    override private function _loadBitmapFromSrc() : BitmapData {
+        var bmp : BitmapData = this._bitmapData;
+        //trace(this._lastSrc);
+        //trace(this._defaultSrc);
+        //trace(this._errorSrc);
+        if ( bmp == null && (this._lastSrc != null || this._defaultSrc!= null)) {
+            bmp = this._setLastBmp(bmp);
+            if( bmp == null ){
+                //Err.trigger('Bitmap not found: ' + this._lastSrc);
+            }//else this.resize(bmp.width, bmp.height);
+        }
+        
+        if (this._updateBitmapCache) {
+            this._updateBitmapCache = false;
+            if (bmp != null) {
+                if (!this._islocalsrc)
+                    this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_WEBIMG));
+                this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_IMG));
+            }else {
+                this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.NONE_IMG));
+            }
+            if (!this.fixedSize) {
+                this._resetSize(bmp);
+            }else {
+                if (bmp != null) {
+                    if (bmp.width > 0) this.scaleX = this.w / bmp.width;
+                    if (bmp.height > 0) this.scaleY = this.h / bmp.height;
+                }     
+            }
+        }
+
+        return bmp;
+    }
+    
+    private function _setLastBmp(bmp : BitmapData) : BitmapData {
+        if (this._lastSrc != null) {
+            bmp = Assets.getBitmapData(this._lastSrc);
+            if (bmp != null) {
+                //trace("_setLastBmp ok");
+                if (this._islocalsrc) this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_LOCALIMG));
+                //else this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_WEBIMG));
+                
+                this._updateBitmapCache = true;
+                return bmp;
+            }
+        }
+        return this._setDefBmp(bmp);
+    }    
+    
+    private function _setDefBmp(bmp : BitmapData) : BitmapData {
+        if (this._defaultSrc != null) {
+            bmp = Assets.getBitmapData(this._defaultSrc);
+            if (bmp != null) {
+                this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_DEFIMG));
+                this._updateBitmapCache = true;
+                //trace("_setDefBmp ok");
+                this._lastSrc = this._defaultSrc;
+            }
+        }
+        return bmp;
+    }
+    
+    private function _setErrBmp(bmp : BitmapData) : BitmapData {
+        if (this._errorSrc != null) {
+            bmp = Assets.getBitmapData(this._errorSrc);
+            if (bmp != null) {
+                //trace("_setErrBmp ok");
+                this.dispatchEvent( new ImageWidgetEvent(ImageWidgetEvent.SHOW_ERRIMG));
+                this._updateBitmapCache = true;
+                this._lastSrc = this._errorSrc;
+                return bmp;
+            }
+        } 
+        return this._setDefBmp(bmp);
+    }
+    
+    private function _resetSize(bmp : BitmapData) : Void {
+        if (bmp != null) {
+            if (this._lastW != bmp.width)
+            {
+                this.w = bmp.width;
+                this._lastW = bmp.width;
+            }
+            if (this._lastW != bmp.height)
+            {
+                this.h = bmp.height;
+                this._lastH = bmp.height;
+            }
+        }else {
+            this._lastW = this.w = 0;
+            this._lastH = this.h = 0;
+        }
+    }
+    
+	private function __onLoaded(event) : Void
+	{
+        //var bmp : BitmapData = this._bitmapData;
+		switch (event.type)
+		{
+            case Start:
+                this._loaded = false;
+                this.dispatchEvent( new URLLoaderEvent(URLLoaderEvent.WEBGET_START));
+                
+            case Cancel:
+                this._progress = 0;
+                this.dispatchEvent( new URLLoaderEvent(URLLoaderEvent.WEBGET_CANCEL));
+                this._loader = null;
+                
+			case Fail(error):
+                this._progress = -1;
+                if (this._errorSrc != null) {
+                    this._bitmapData = null;
+                    this._lastSrc = this._errorSrc;
+                }else if (this._defaultSrc != null ) {
+                    this._bitmapData = null;
+                    this._lastSrc = this._defaultSrc;
+                }
+                this.dispatchEvent( new URLLoaderEvent(URLLoaderEvent.WEBGET_FAIL));
+                this._updateBitmapCache = true;
+                this.refresh();
+                this._loader = null;
+                this._loaded = false;
+
+			case Complete:
+                this._progress = 1;
+				this._bitmapData = event.target.content;
+                this.dispatchEvent( new URLLoaderEvent(URLLoaderEvent.WEBGET_COMPLETE));
+                this._islocalsrc = false;
+                this._updateBitmapCache = true;
+                this.refresh();
+                this._loader = null;
+                this._loaded = true;
+                
+            case Progress:
+                //var p = Std.int(this._loader.progress*100);
+                this._progress = this._loader.progress;
+                this.dispatchEvent( new URLLoaderEvent(URLLoaderEvent.WEBGET_PROGRESS));
+
+			default:
+		}
+	}  
+    
+}
